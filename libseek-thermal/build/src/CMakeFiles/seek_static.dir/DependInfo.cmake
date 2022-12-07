cf093553d2bcd09689ff735d58bebaac50c33207
MichaelSartori <72945883+MichaelSartori@users.noreply.github.com>
2022-12-07 11:32:19 +0100

    Add files via upload

---
+++ libseek-thermal/build/src/CMakeFiles/seek_static.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vm/Treiber Seek/libseek-thermal/src/SeekCam.cpp" "/home/vm/Treiber Seek/libseek-thermal/build/src/CMakeFiles/seek_static.dir/SeekCam.cpp.o"
+  "/home/vm/Treiber Seek/libseek-thermal/src/SeekDevice.cpp" "/home/vm/Treiber Seek/libseek-thermal/build/src/CMakeFiles/seek_static.dir/SeekDevice.cpp.o"
+  "/home/vm/Treiber Seek/libseek-thermal/src/SeekThermal.cpp" "/home/vm/Treiber Seek/libseek-thermal/build/src/CMakeFiles/seek_static.dir/SeekThermal.cpp.o"
+  "/home/vm/Treiber Seek/libseek-thermal/src/SeekThermalPro.cpp" "/home/vm/Treiber Seek/libseek-thermal/build/src/CMakeFiles/seek_static.dir/SeekThermalPro.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libusb-1.0"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
