a968b6b37824933a92bbc9228914f9f1e23a9171
MichaelSartori <72945883+MichaelSartori@users.noreply.github.com>
2022-12-07 11:34:21 +0100

    Add files via upload

---
+++ libseek-thermal/examples/CMakeFiles/seek_viewer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vm/Treiber Seek/libseek-thermal/examples/seek_viewer.cpp" "/home/vm/Treiber Seek/libseek-thermal/build/examples/CMakeFiles/seek_viewer.dir/seek_viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/include/opencv"
+  "/usr/include/libusb-1.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vm/Treiber Seek/libseek-thermal/build/src/CMakeFiles/seek_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
